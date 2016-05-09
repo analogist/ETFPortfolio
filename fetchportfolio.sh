5d2b7306e3ffe32070ad628cb2831e8542d1cc89
James Wu <james@analogist.net>
2016-05-09 16:59:06 -0700

    add fetch script and shares initialization

---
+++ fetchportfolio.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+wget -O portfolio.csv "http://finance.yahoo.com/d/quotes.csv?s=VNQ+BND+VTI+VXF+VXUS+VHT&f=sl1m3m4"
---
+++ purchased.js
@@ -0,0 +1,8 @@
+purchased = {
+VNQ: 2,
+BND: 15,
+VTI: 40,
+VXF: 17,
+VXUS: 15,
+VHT: 3
+};
