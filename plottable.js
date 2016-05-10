548776a970b57803ace069c1e5260d06fbeec769
James Wu <james@analogist.net>
2016-05-09 21:57:11 -0700

    globalize table formatting functions

--- plottable.js
+++ plottable.js
@@ -1,4 +1,21 @@
+var perc = d3.format("0.1%");
+var money = d3.format("$0.2f");
+var tableheaders = [];
+
+var formattable = function(d, i) {
+    if(i == 0)
+        return d.value;
+    else if(i == 1)
+        return money(d.value);
+    else if(i == 2)
+        return perc(d.value);
+    else if(i == 3)
+        return d.value;
+}
+
 var dataconvert = function(data, usemetric){
+    tableheaders = ["ETF", usemetric, "Percent", "Shares"];
+
     datasum = d3.sum(
             data.map(function(d) {
                 return d[usemetric]*purchased[d.ETF];
@@ -15,39 +32,25 @@ var dataconvert = function(data, usemetric){
     });
 }
 
-var perc = d3.format("0.1%");
-var money = d3.format("$0.2f");
-
 var maketable = function(data_in, usemetric) {
+
     recomputeddata = dataconvert(data_in, usemetric);
 
-    var table = d3.select("body")
-        .append("table");
+    var table = d3.select("table#sharestable")
+
     table.append("thead").append("tr")
         .selectAll("th")
-        .data(["ETF", usemetric, "Percent", "Shares"])
-        .enter()
-        .append("th")
+        .data(tableheaders)
+        .enter().append("th")
             .text(function(d) {return d;});
     var rows = table.append("tbody")
         .selectAll("tr")
         .data(recomputeddata)
         .enter()
-        .append("tr");
-    var cells = rows.selectAll("td")
+        .append("tr")
+        .selectAll("td")
         .data(function(d) { return d3.entries(d); })
-        .enter()
-        .append("td")
-            .text(function(d,i) {
-                if(i == 0)
-                    return d.value;
-                else if(i == 1)
-                    return money(d.value);
-                else if(i == 2)
-                    return perc(d.value);
-                else if(i == 3)
-                    return d.value;
-                    
-            })
+        .enter().append("td")
+            .text(function(d, i) { return formattable(d, i); })
 
 }
